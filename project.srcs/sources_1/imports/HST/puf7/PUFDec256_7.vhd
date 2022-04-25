896e5dc5911ba6af403d164bd004884ed37b38b6
Nicole Baldy <neb45@uakron.edu>
2022-04-25 14:56:20 -0400

    Add Luke's files

---
+++ project.srcs/sources_1/imports/HST/puf7/PUFDec256_7.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.ALL;
+
+entity PUFDec256_7 is
+port(
+	i_Sel : in std_logic_vector(7 downto 0);
+	o_Q : out std_logic_vector(255 downto 0) -- Data value output
+);
+end PUFDec256_7;
+
+architecture behavior of PUFDec256_7 is
+
+begin
+
+
+
+	--gen_ro_and_counters: for i in 0 to 511 generate
+gener: for i in 0 to 255 generate
+	process (i_Sel)
+		begin
+			if( i = unsigned(i_Sel)) then
+				o_Q(i) <= '1';
+			else
+				o_Q(i) <= '0';
+		end if;	
+
+	end process;
+end generate;	
+--Possible alternate solution	
+
+
+-- o_Q <= (i_Sel=>'1', OTHERS=>'0');	
+
+end behavior;
